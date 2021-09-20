1c9b4d91cf19b35f05d293d2015e383a34cb6ea2
Buğra Altıntaş <bugraltintas@gmail.com>
2021-09-20 17:00:45 +0300

    feat(components): Footer and Header elements added

--- src/components/Elements/index.js
+++ src/components/Elements/index.js
@@ -1,5 +1,7 @@
 export { default as section } from './Section';
 export { default as yigit } from './Yigit';
+export { default as header } from './Header';
 export { default as about } from './AboutUs';
 export { default as cardlist } from './CardList';
 export { default as productlist } from './ProductList';
+export { default as footer } from './Footer';
