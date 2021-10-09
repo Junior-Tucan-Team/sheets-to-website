458a5149ac6508bfa479638030ae5a88b5dabad8
Mert Kaan YILMAZ <e238109@metu.edu.tr>
2021-10-09 22:17:34 +0300

    chore(LoginPage): login pop up completed

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -21,11 +21,31 @@ function LoginPage(props) {
   };
 
   const changePopUp = (e) => {
+    const loginForm = document.getElementsByClassName('login-form');
+    const gglButton = document.getElementsByClassName('google-button');
+    const fcbBUtton = document.getElementsByClassName('facebook-button');
+    const hrznLine = document.getElementsByClassName('horizontal-line');
+    const orText = document.getElementsByClassName('or-text');
+    const chngmdText = document.getElementsByClassName('change-mod-text');
     e.preventDefault();
     if (e.target.value === 'log-in') {
       setSignedUp('sign-in');
+      loginForm[0].style.height = '500px';
+      gglButton[0].style.top = '149px';
+      fcbBUtton[0].style.top = '210px';
+      hrznLine[0].style.top = '295px';
+      hrznLine[1].style.top = '295px';
+      orText[0].style.top = '285px';
+      chngmdText[0].style.top = '421px';
     } else {
       setSignedUp('log-in');
+      loginForm[0].style.height = '665px';
+      gglButton[0].style.top = '117px';
+      fcbBUtton[0].style.top = '180px';
+      hrznLine[0].style.top = '261px';
+      hrznLine[1].style.top = '261px';
+      orText[0].style.top = '251px';
+      chngmdText[0].style.top = '588px';
     }
   };
 
@@ -35,7 +55,8 @@ function LoginPage(props) {
         signedUp === 'log-in' ?
           <span className="welcome-back">Welcome Back!</span> :
           <div>
-            <span>Please sign up to start collecting data</span>
+            <img className="jotform-logo" src="https://cdn.jotfor.ms/assets/img/logo2021/jotform-logo.svg" alt="JotForm Logo"/>
+            <span className="jotform-logo-header">Please sign up to start collecting data</span>
           </div>
       }
       <button className="signed-up-button google-button">
@@ -43,33 +64,38 @@ function LoginPage(props) {
         {signedUp === 'log-in' ? 'Log in ' : 'Sign up '} with Google
       </button>
       <button className="signed-up-button facebook-button">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2048px-Google_%22G%22_Logo.svg.png" alt="Google Logo" />
+        <img src="https://nrgteleresources.com/wp-content/uploads/facebook-logo-white.png" alt="Google Logo" />
         {signedUp === 'log-in' ? 'Log in ' : 'Sign up '} with Facebook
       </button>
-      <div style={{ marginTop: '27px' }}>
-        <div className="horizontal-line"/>
-        <span style={{ fontSize: '16px', lineHeight: '20px', color: '#A6A6A6' }}>OR</span>
-        <div className="horizontal-line"/>
-      </div>
-      <div className="horizontal-line"/>
       <div>
-        <label>Username of Email</label>
-        <br/>
-        <input
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
+        <div className="horizontal-line" id="left-line"/>
+        <span className="or-text">OR</span>
+        <div className="horizontal-line" id="right-line"/>
       </div>
-      <div>
-        <label>Password</label>
-        <br/>
-        <input
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-      </div>
+      {
+        signedUp === 'log-in' ?
+          <div>
+            <label className="form-input-absolute" id="above-login-label">Username or Email</label>
+            <br/>
+            <input
+              className="form-input-absolute"
+              id="above-login-input"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <label className="form-input-absolute" id="bottom-login-label">Password</label>
+            <br/>
+            <input
+              className="form-input-absolute"
+              id="bottom-login-input"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </div> :
+        null
+      }
       {
         signedUp === 'log-in' ?
           <div>
@@ -81,7 +107,10 @@ function LoginPage(props) {
             </span>
           </div> :
           <div>
-            <button className="signed-up-button sign-up-wmail">Sign in with Email</button>
+            <button className="signed-up-button sign-up-wmail">
+              <img src="https://toppng.com/uploads/preview/icon-mail-png-transparent-background-mail-logo-11562851894ksatrtd2da.png" alt="mail logo"/>
+              Sign in with Email
+            </button>
             <br/>
             <span className="change-mod-text">
               Already have an account?
