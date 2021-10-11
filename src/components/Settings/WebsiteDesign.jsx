78f9cd0b71aa69813643c72ffb2b845ad567ed36
Emre Şahin <emreshn00@outlook.com>
2021-10-12 02:53:18 +0300

    fix(website design properties): styling

--- src/components/Settings/WebsiteDesign.jsx
+++ src/components/Settings/WebsiteDesign.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Styles/textBox.css';
+import './Styles/websiteDesign.css';
 
 
 const WebsiteDesign = () => {
@@ -35,27 +35,30 @@ const WebsiteDesign = () => {
 
   return (
     <div>
+      <div className="website-basic-design">BASIC DESIGN</div>
       <div className="logo-handler-source-type">WEBSITE BACKGROUND</div>
-      <div className="website-background-setting">
-        <label htmlFor="website-background-color">Website Background Color</label>
-        <input
-          id="website-background-color"
-          type="color"
-          onChange={(e) => {
-            layout.style.backgroundColor = e.target.value;
-        }}
-        />
-      </div>
-      <div className="image-handler">
-        <div>Background Image</div>
-        <button className="choose-a-file-button" onClick={() => setIsOpen(true)}>Choose a file</button>
-        {isOpen ?
-          <div className="image-handler">
-            <div className="upload-type-box">
-              {renderTabs()}
-              <div className="logo-box-upload" >{renderImage()}</div>
-            </div>
-          </div> : <></>}
+      <div className="website-design-property">
+        <div className="website-background-setting">
+          <label htmlFor="website-background-color">Background Color</label>
+          <input
+            id="website-background-color"
+            type="color"
+            onChange={(e) => {
+              layout.style.backgroundColor = e.target.value;
+          }}
+          />
+        </div>
+        <div className="website-background-image-handler">
+          <div>Background Image</div>
+          <button className="choose-a-file-button" onClick={() => setIsOpen(true)}>CHOOSE A FILE</button>
+          {isOpen ?
+            <div className="image-handler">
+              <div className="upload-type-box">
+                {renderTabs()}
+                <div className="logo-box-upload" >{renderImage()}</div>
+              </div>
+            </div> : <></>}
+        </div>
       </div>
     </div>
 
