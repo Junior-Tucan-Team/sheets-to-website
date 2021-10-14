87f7c1f6181667915c6c8cd7abf7bbace15d9d67
Emre Şahin <48887157+mrshn@users.noreply.github.com>
2021-10-14 17:40:51 +0300

    fix(productList):created from strech

--- src/components/Elements/ProductList.jsx
+++ src/components/Elements/ProductList.jsx
@@ -10,39 +10,27 @@ const Component = ({ item, onClick }) => {
       data-element-id={item.id}
       onClick={onClick}
     >
-      <div className="container px-5 py-24 mx-auto grid grid-cols-3 gap-4">
-        <div className="w-full mb-20 header-grid">
-          <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900" style={{ color: item.fntclrtitle, fontSize: item.fntSztitle, fontFamily: item.fntfmlytitle }}>{item.title}</h1>
-            <div className="h-1 w-20 bg-indigo-500 rounded" />
-          </div>
-          <p className="lg:w-1/2 w-full leading-relaxed text-gray-500" style={{ color: item.fntclrdescription, fontSize: item.fntSzdescription, fontFamily: item.fntfmlydescription }}> {item.description}</p>
+      <div className="container grid grid-cols-3 gap-4 container-god">
+        <div className="header-grid">
+          <h1 className="">{item.title}</h1>
         </div>
         {typeof submissions !== 'undefined' ? submissions.map(submission => (
-          <div className="flex submissionss-center lg:w-3/5 mx-auto border-b pb-10 mb-10
-              border-gray-200 sm:flex-row flex-col"
-          >
-            <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex
-            submissionss-center justify-center
-            rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0"
-            >
+          <div className="container-each-card">
+            <div className="container-each-card-img-div">
               { item.source.image !== '' ? <img
-                className="h-40 rounded w-full
-              object-cover object-center mb-6"
+                className="container-each-card-img-div-img"
                 src={submission.answers[item.source.image].answer}
                 alt="content"
               /> : <></>}
             </div>
-            <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-              <h2 className="text-gray-900 text-lg title-font
-              font-medium mb-2"
-              >{item.source.header !== '' ? submission.answers[item.source.header].answer : ''}
+            <div className="container-each-card-text-div">
+              <h2 className="">{item.source.header !== '' ? submission.answers[item.source.header].answer : ''}
               </h2>
-              <p className="leading-relaxed text-base">
+              <p className="container-each-card-text-div-p">
                 {item.source.description !== '' ? submission.answers[item.source.description].answer : ''}
               </p>
-              <p className="mt-1">{item.source.price !== '' ? submission.answers[item.source.price].answer : ''}
-              </p>
+            </div>
+            <div className="container-each-card-price">{item.source.price !== '' ? submission.answers[item.source.price].answer : ''}
             </div>
           </div>)) : <></>}
       </div>
